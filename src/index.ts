b48c7d5601091a2502283d08ee8a57aa92308b28
tkgstrator <nasawake.am@gmail.com>
2023-09-05 16:45:05 +0900

    restore features

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
-// import { Hash } from 'crypto';
-// import { EnumURLType } from './dto/internal_type';
-// import { LocaleType } from './dto/locale_type';
-// import { Translation } from './dto/translation';
-// import { Version , URLType } from './dto/urls';
-// import { OutputType, SwiftEnumWriter } from './utils/enum';
+import { EnumURLType } from './dto/internal_type';
+import { LocaleType } from './dto/locale_type';
+import { Translation } from './dto/translation';
+import { Version , URLType } from './dto/urls';
+import { OutputType, SwiftEnumWriter } from './utils/enum';
 import { SHA256Hash } from './dto/hashes';
 
-// const locales: LocaleType[] = await LocaleType.all_cases();
+const locales: LocaleType[] = await LocaleType.all_cases();
 
-// // 翻訳の出力
-// locales.forEach(async (locale: LocaleType) => {
-//   const translation: Translation = await locale.get_translation();
-//   translation.write();
-// });
+// 翻訳の出力
+locales.forEach(async (locale: LocaleType) => {
+  const translation: Translation = await locale.get_translation();
+  translation.write();
+});
 
-// // Enumの出力
-// Object.values(URLType).forEach(async (url: URLType) => {
-//   const data: EnumURLType = await EnumURLType.from(url, Version.V400);
-//   const writer = new SwiftEnumWriter(data);
+// Enumの出力
+Object.values(URLType).forEach(async (url: URLType) => {
+  const data: EnumURLType = await EnumURLType.from(url, Version.V400);
+  const writer = new SwiftEnumWriter(data);
 
-//   Object.values(OutputType).forEach((type) => {
-//     writer.write(type);
-//   });
-// });
+  Object.values(OutputType).forEach((type) => {
+    writer.write(type);
+  });
+});
 
 // SHA256Hashの出力
 console.log(await SHA256Hash.write())
